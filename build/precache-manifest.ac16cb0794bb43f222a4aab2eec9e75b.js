d95aecaf8ffa861fe01908339b3c51f4e2f0e01b
quentin roux <quentin.roux@hotmail.fr>
2020-02-09 23:26:44 +0100

    working app with express server

---
+++ build/precache-manifest.ac16cb0794bb43f222a4aab2eec9e75b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2790d5060a013937e3f13189fa7abdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eccd4e8051c304fa85a",
+    "url": "/static/js/2.ede892fc.chunk.js"
+  },
+  {
+    "revision": "c0f4aa119942e5c1f85dd4acb785bdb8",
+    "url": "/static/js/2.ede892fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ae3fb2a642f21c863e",
+    "url": "/static/js/main.1f1f93c7.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
